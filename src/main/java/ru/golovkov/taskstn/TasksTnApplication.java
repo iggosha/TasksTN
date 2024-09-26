978cf87b9d245980116d19c799c458b35de553b3
iggosha <iegolovkov@gmail.com>
2024-09-26 15:05:45 +0300

    Создание проекта

---
+++ src/main/java/ru/golovkov/taskstn/TasksTnApplication.java
@@ -0,0 +1,13 @@
+package ru.golovkov.taskstn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksTnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasksTnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/golovkov/taskstn/TasksTnApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.golovkov.taskstn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksTnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
